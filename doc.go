9535a6cd233dfe50d8459e9a72996fa9845ac9e3
James Gray <james@james4k.com>
2012-08-26 23:03:52 -0500

    added a comment about client-side javascript

--- doc.go
+++ doc.go
@@ -7,6 +7,8 @@ of its methods and considers those that are prefixed with "On" to be event
 handlers. These event handlers are called when events are received from the
 client. For every new WebSocket connection, a new instance of the struct is
 created.
+
+Note: A simple JavaScript component still needs to be written for the client-side.
 */
 package wsevents
 
